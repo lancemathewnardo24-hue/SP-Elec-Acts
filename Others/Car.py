7e6edd4ef1eadcdb6a5ed29df49d9b6055555124
Lance <lancemathewnardo24@gmail.com>
2026-02-11 17:20:03 +0800

    commit

---
+++ Others/Car.py
@@ -0,0 +1,17 @@
+class Car:
+    brand = ''
+    model = ''
+    numofwheels = 0
+    fueltype = ''
+    remainingfuel = 0.0
+
+    def run(car, fuelConsumed):
+        if self.remaningfuel < fuelConsumed:
+            print("The car will not run, No Gas remaining")
+        else:
+            self.remainingfuel -= fuelConsumed
+
+
+    def gasUp(car, amount):
+
+    def displayDetails
